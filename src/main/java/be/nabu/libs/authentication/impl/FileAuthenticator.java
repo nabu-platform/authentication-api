77c607c455f16b75d1e7345626900c3ede2de7ed
alex <alex@nabu.be>
2015-08-21 08:48:10 +0200

    Updated file authenticator to allow for a root

--- src/main/java/be/nabu/libs/authentication/impl/FileAuthenticator.java
+++ src/main/java/be/nabu/libs/authentication/impl/FileAuthenticator.java
@@ -7,9 +7,17 @@ import java.io.InputStream;
 
 public class FileAuthenticator extends BasePasswordAuthenticator {
 	
+	private File root;
+
+	public FileAuthenticator() {}
+	
+	public FileAuthenticator(File root) {
+		this.root = root;
+	}
+	
 	@Override
 	protected InputStream getInput(String fileName) throws IOException {
-		File file = new File(fileName);
+		File file = root == null ? new File(fileName) : new File(root, fileName);
 		return file.exists() && file.isFile() ? new FileInputStream(file) : null;
 	}
 	
