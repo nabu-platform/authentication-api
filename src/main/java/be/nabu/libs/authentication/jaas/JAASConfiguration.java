c26535caf0c4988d5f4015c8f8824ac1cc5c1786
alex <alex@nabu.be>
2016-03-10 16:28:13 +0100

    Added a jaas configuration extension for more control

---
+++ src/main/java/be/nabu/libs/authentication/jaas/JAASConfiguration.java
@@ -0,0 +1,59 @@
+package be.nabu.libs.authentication.jaas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.Configuration;
+import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag;
+
+public class JAASConfiguration extends Configuration {
+
+	private static boolean registered = false;
+	private static JAASConfiguration instance;
+	
+	static {
+		instance = new JAASConfiguration(Configuration.getConfiguration());
+	}
+	
+	public static void register() {
+		if (!registered) {
+			synchronized(JAASConfiguration.class) {
+				if (!registered) {
+					Configuration.setConfiguration(instance);
+					registered = true;
+				}
+			}
+		}
+	}
+	
+	public static JAASConfiguration getInstance() {
+		return instance;
+	}
+	
+	private Configuration parent;
+	private Map<String, AppConfigurationEntry[]> entries = new HashMap<String, AppConfigurationEntry[]>();
+
+	private JAASConfiguration(Configuration parent) {
+		this.parent = parent;
+	}
+	
+	@Override
+	public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
+		if (entries.containsKey(name)){
+			return entries.get(name);
+		}
+		else if (parent != null) {
+			return parent.getAppConfigurationEntry(name);
+		}
+		return null;
+	}
+
+	public void configure(String name, AppConfigurationEntry...entries) {
+		this.entries.put(name, entries);
+	}
+	
+	public static AppConfigurationEntry newKerberosEntry(Map<String, String> options) {
+		return new AppConfigurationEntry("com.sun.security.auth.module.Krb5LoginModule", LoginModuleControlFlag.REQUIRED, options);
+	}
+}
