b437e3accd2ec707a0418cf63ef70b9493ffe50d
alex <alex@nabu.be>
2016-10-15 08:38:20 +0200

    Updated device validator

--- src/main/java/be/nabu/libs/authentication/api/DeviceValidator.java
+++ src/main/java/be/nabu/libs/authentication/api/DeviceValidator.java
@@ -4,8 +4,6 @@ import javax.jws.WebParam;
 import javax.jws.WebResult;
 
 public interface DeviceValidator {
-	@WebResult(name = "deviceId")
-	public String newDeviceId(@WebParam(name = "token") Token token, @WebParam(name = "remoteIp") String remoteIp, @WebParam(name = "deviceDescription") String deviceDescription);
 	@WebResult(name = "isAllowed")
-	public Boolean isAllowed(@WebParam(name = "token") Token token, @WebParam(name = "remoteIp") String remoteIp, @WebParam(name = "deviceId") String deviceId);
+	public boolean isAllowed(@WebParam(name = "token") Token token, @WebParam(name = "device") Device device);
 }
