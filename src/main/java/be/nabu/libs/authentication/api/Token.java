8e06aa3a5c73a7a2626927ec38aeec1b60516964
Alexander Verbruggen <alex@nabu.be>
2021-02-06 11:30:40 +0100

    added support for id

--- src/main/java/be/nabu/libs/authentication/api/Token.java
+++ src/main/java/be/nabu/libs/authentication/api/Token.java
@@ -22,4 +22,14 @@ public interface Token extends Principal, Serializable {
 	 * The original list of credentials
 	 */
 	public List<Principal> getCredentials();
+	/**
+	 * A globally unique and _anonymous_ (GDPR and all that) id
+	 * This can be used to store security settings without knowing the exact context
+	 * For example logging the owner, without knowing where he exists (CMS, SSO,...)
+	 * This will almost always be a UUID, but to leave the door open to other generation schemes, it is defined as a string
+	 * At higher levels we can enforce the uuid-ness of this id if relevant
+	 */
+	public default String getId() {
+		return null;
+	}
 }
